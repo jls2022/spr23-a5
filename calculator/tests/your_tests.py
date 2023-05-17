8d4a7233004ae3eb1884f38c81db64d7bb3d6ec1
jls2022 <jael2345@hotmail.com>
2023-05-16 20:17:25 -0700

    complete MakeFile

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+
+#checks that the program outputs "3" for an input of "1 - 1"
+assert run("1 - 1").output == "0"
+# Checks that the program fails (correctly errors) for input "1 & 2"
+assert run("1 & 2").exit_status != 0
 
 
 ###
